f129f9761da5a2ace54a80f3202d79c4a18abbbc
cwoolnoughchamorro <cwoolnoughchamorro@googlemail.com>
2018-10-08 08:53:06 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,24 @@
-# your code goes here
+def begins_with_r(tools)
+    tools.all? do |string|
+    string[0] == "r"
+  end
+end
+
+
+def contain_a(string)
+    contain_a_new = []
+    string.each do |word|
+    if word.include?('a')
+      contain_a_new.push(word)
+    end
+  end
+  contain_a_new
+end
+
+def first_wa(string)
+  string.find { |word| word.to_s.start_with?('wa')}
+end
+
+def remove_non_strings(array)
+  array.grep(String)
+end
